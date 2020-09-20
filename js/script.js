a968707fc7ba8c987c34e297c8a32942d0bf9737
Rafsun Sakib <68976031+RafsunSakib@users.noreply.github.com>
2020-09-20 22:22:00 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    var mixer = mixitup('.mymixcont');
+});
+
+$(document).ready(function(){
+    var typed = new Typed(".type",{
+        strings:["Web Designer","UI/UX Designer","Security Researcher"],
+        typeSpeed:60,
+        backSpeed:60,
+        loop:true
+    });
+});
+
